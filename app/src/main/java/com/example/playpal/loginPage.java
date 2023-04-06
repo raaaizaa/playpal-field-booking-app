d012e6fe04a5779528ab2a9811ad5fc7683ea16e
raaizar <rahmanraiza@gmail.com>
2023-04-06 20:06:20 +0700

    added a few backends such as validator & fixing xml stuffs

--- app/src/main/java/com/example/playpal/loginPage.java
+++ app/src/main/java/com/example/playpal/loginPage.java
@@ -2,13 +2,84 @@ package com.example.playpal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class loginPage extends AppCompatActivity {
+
+    private ImageView google, facebook, apple;
+    private TextView registerHere, forgotPass;
+    private EditText email, password;
+    private Button login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_page);
+        initialize();
+    }
+
+    public void initialize(){
+        email = findViewById(R.id.emailLoginField);
+        password = findViewById(R.id.passwordLoginField);
+
+        login = findViewById(R.id.loginButton);
+        setLogin();
+
+        forgotPass = findViewById(R.id.forgotPass);
+        setForgotPass();
+
+        registerHere = findViewById(R.id.registerHereText);
+        setRegisterHere();
+
+        google = findViewById(R.id.loginUsingGoogle);
+        facebook = findViewById(R.id.loginUsingFacebook);
+        apple = findViewById(R.id.loginusingAppleID);
+        setLoginUsing();
+    }
+
+    public void setLogin(){
+        login.setOnClickListener(e -> {
+            String inputtedEmail = email.getText().toString();
+            String inputtedPassword = password.getText().toString();
+
+            // Yang pasti disini validasi, masuk ke database, dll
+        });
+    }
+
+    public void setForgotPass(){
+        forgotPass.setOnClickListener(e -> {
+            Toast.makeText(this, "Sorry, this service currently not available!", Toast.LENGTH_SHORT).show();
+        });
+    }
+
+    public void setRegisterHere(){
+        registerHere.setOnClickListener(e -> {
+            openRegisterPage();
+        });
+    }
+
+    public void setLoginUsing(){
+        google.setOnClickListener(e -> {
+            Toast.makeText(this, "Sorry, this service currently not available!", Toast.LENGTH_SHORT).show();
+        });
+
+        facebook.setOnClickListener(e -> {
+            Toast.makeText(this, "Sorry, this service currently not available!", Toast.LENGTH_SHORT).show();
+        });
+
+        apple.setOnClickListener(e -> {
+            Toast.makeText(this, "Sorry, this service currently not available!", Toast.LENGTH_SHORT).show();
+        });
+    }
+
+    public void openRegisterPage(){
+        Intent intent = new Intent(this, registerPage.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/playpal/registerPage.java
+++ app/src/main/java/com/example/playpal/registerPage.java
@@ -2,13 +2,96 @@ package com.example.playpal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class registerPage extends AppCompatActivity {
+
+    private ImageButton back;
+    private Button register;
+    private EditText email, username, password, confirm;
+    private TextView loginHere;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_page);
+
+        initialize();
     }
+
+    public void initialize(){
+        email = findViewById(R.id.emailRegisterField);
+        username = findViewById(R.id.usernameRegisterField);
+        password = findViewById(R.id.passwordRegisterField);
+        confirm = findViewById(R.id.confirmRegisterField);
+
+        register = findViewById(R.id.registerButton);
+        setRegister();
+
+        back = findViewById(R.id.backButton);
+        setBackButton();
+
+        loginHere = findViewById(R.id.loginHereText);
+        setLoginHere();
+    }
+
+    public void setBackButton(){
+        back.setOnClickListener(e -> {
+            openLoginPage();
+        });
+    }
+
+    public void setLoginHere(){
+        loginHere.setOnClickListener(e -> {
+            openLoginPage();
+        });
+    }
+
+    public void setRegister(){
+        register.setOnClickListener(e -> {
+            String inputtedEmail = email.getText().toString();
+            String inputtedUsername = username.getText().toString();
+            String inputtedPass = password.getText().toString();
+            String inputtedConfirm = confirm.getText().toString();
+
+            if(inputtedEmail.isEmpty() || inputtedUsername.isEmpty() || inputtedPass.isEmpty() || inputtedConfirm.isEmpty()){
+                Toast.makeText(this, "All fields must be filled!", Toast.LENGTH_SHORT).show();
+            }else if(!Patterns.EMAIL_ADDRESS.matcher(inputtedEmail).matches()){
+                Toast.makeText(this, "Invalid email address!", Toast.LENGTH_SHORT).show();
+            }else if(!validator((inputtedPass))){
+                Toast.makeText(this, "Password must be alphanumeric!", Toast.LENGTH_SHORT).show();
+            }else if(!inputtedPass.equals(inputtedConfirm)){
+                Toast.makeText(this, "Password doesn't match!", Toast.LENGTH_SHORT).show();
+            }else{
+                // Disini pokonya nambahin data ke database, gatau caranya ntar ajadeh
+                Toast.makeText(this, "Register Success!", Toast.LENGTH_SHORT).show();
+                openLoginPage();
+            }
+        });
+    }
+
+    public void openLoginPage(){
+        Intent intent = new Intent(this, loginPage.class);
+        startActivity(intent);
+    }
+
+    public static boolean validator(String string){
+        String regex = "^(?=.*[a-zA-Z])(?=.*[0-9])[A-Za-z0-9]+$";
+        Pattern p = Pattern.compile(regex);
+        Matcher m = p.matcher(string);
+
+        return m.matches();
+    }
+
+
 }
