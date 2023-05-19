268a8eae5bf2d877fcd38473a2887a0e0941450e
raaizar <rahmanraiza@gmail.com>
2023-05-19 13:49:35 +0700

    success implementing google maps api

---
+++ app/src/main/java/com/example/playpal/activities/maps.java
@@ -0,0 +1,28 @@
+package com.example.playpal.activities;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.playpal.R;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+
+public class maps extends AppCompatActivity implements OnMapReadyCallback{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.maps);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+
+    }
+}
